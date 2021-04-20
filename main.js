89a35e049a38cd62dc34bc54d45f0b2bb05c6d6c
CHOCODULCITOS <82556657+CHOCODULCITOS@users.noreply.github.com>
2021-04-20 17:48:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('activo');
+});
+
+
+ScrollReveal().reveal('.cabecera');
+ScrollReveal().reveal('.nueva-card',{delay:500});
+ScrollReveal().reveal('.cards-banner-once',{delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
+ScrollReveal().reveal('.cards-banner-3',{delay:500});
